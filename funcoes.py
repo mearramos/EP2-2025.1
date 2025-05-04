76c6688eaf0a0c27f6e2b9d69f287455fecedb1c
Maria Ramos <mariaramossilva901@gmail.com>
2025-05-04 15:25:52 -0300

    Implmentação da função guardar_dado

--- funcoes.py
+++ funcoes.py
@@ -8,4 +8,22 @@ def rolar_dados(inteiro):
         i += 1
     return retorno
 
+def guardar_dado(rolados, guardados, indice):
+    retorno = []
+
+    escolhido = rolados[indice]
+    guardados.append(escolhido)
+    del rolados[indice]
+
+    retorno.append(rolados)
+    retorno.append(guardados)
+
+
+    return retorno
+
+# dados_rolados =  [6, 1, 6, 4]
+# dados_no_estoque = [2]
+# dado_para_guardar = 2
+
+# print(guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar))
 
