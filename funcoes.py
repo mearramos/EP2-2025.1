717c62b29b89f90e1aa99d0176ec1b18f023bd73
Maria Ramos <mariaramossilva901@gmail.com>
2025-05-04 15:36:29 -0300

    Implmentação da função remover_dado

--- funcoes.py
+++ funcoes.py
@@ -27,3 +27,20 @@ def guardar_dado(rolados, guardados, indice):
 
 # print(guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar))
 
+def remover_dado(rolados, guardados, removido):
+    retorno = []
+
+    rolados.append(guardados[removido])
+    del guardados[removido]
+
+    retorno.append(rolados)
+    retorno.append(guardados)
+
+    return retorno
+
+# dados_rolados = [2, 2, 2, 2]
+# dados_no_estoque = [1]
+# dado_para_remover = 0
+
+# print(remover_dado(dados_rolados, dados_no_estoque, dado_para_remover))
+    
